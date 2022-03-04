51363f57f7f4319313b96c30d14fc56684c6aea3
xinyih13 <xinyih13@uci.edu>
2022-03-03 19:05:52 -0800

    gallery activity updated

--- app/src/main/java/com/example/locofoco/CatImage.kt
+++ app/src/main/java/com/example/locofoco/CatImage.kt
@@ -3,6 +3,7 @@ package com.example.locofoco
 import android.os.Parcelable
 import kotlinx.parcelize.IgnoredOnParcel
 import kotlinx.parcelize.Parcelize
+import org.json.JSONArray
 
 
 @Parcelize
@@ -10,7 +11,10 @@ class CatImage(val url : String) : Parcelable {
     @IgnoredOnParcel
     val imageUrl = url
 
+    //val imageId = id
+
     //just trying to show some images (can be removed later)
+    //helper function
     companion object {
         fun loadImages() : List<CatImage>{
             val images = mutableListOf<CatImage>()
@@ -22,6 +26,17 @@ class CatImage(val url : String) : Parcelable {
             images.add(CatImage(url))
             images.add(CatImage(url))
             images.add(CatImage(url))
+            return images
+        }
+
+        fun fromJsonArray(JsonArray: JSONArray): List<CatImage> {
+            val images = mutableListOf<CatImage>()
+            for (i in 0 until JsonArray.length()){
+                val imageJson = JsonArray.getJSONObject(i)
+                images.add(CatImage(
+                    imageJson.getString("url"),
+                ))
+            }
             return images
         }
     }
