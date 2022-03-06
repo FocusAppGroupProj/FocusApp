e85a51243438c1af448f729d25c8e8b725721043
duonghv1 <thuyduong8122002@gmail.com>
2022-03-05 23:36:37 -0800

    Updated GalleryAcitivity

--- app/src/main/java/com/example/locofoco/GalleryActivity.kt
+++ app/src/main/java/com/example/locofoco/GalleryActivity.kt
@@ -7,11 +7,11 @@ import androidx.recyclerview.widget.GridLayoutManager
 
 
 
-
 class GalleryActivity : AppCompatActivity() {
     lateinit var rvGallery : RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_gallery)
 
         rvGallery = findViewById(R.id.rvGallery)
