c162b75c22ec042a53c9f5f2c068d16b9b96af59
waterFalt777 <stephy888la7@gmail.com>
2022-03-16 16:44:58 -0700

    fixed color

--- app/src/main/java/com/example/locofoco/MainActivity.kt
+++ app/src/main/java/com/example/locofoco/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.locofoco
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+//import android.os.FileUtils
 import android.util.Log
 import android.widget.Button
 import com.codepath.asynchttpclient.AsyncHttpClient
