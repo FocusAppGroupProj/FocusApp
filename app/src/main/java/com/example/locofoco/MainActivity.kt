83550b236ce5af7d24b1c56ca66cbfaa82c0d458
waterFalt777 <stephy888la7@gmail.com>
2022-03-16 09:17:59 -0700

    fixed anim bug

--- app/src/main/java/com/example/locofoco/MainActivity.kt
+++ app/src/main/java/com/example/locofoco/MainActivity.kt
@@ -53,13 +53,12 @@ class MainActivity : AppCompatActivity() {
 
         }
     //DONT DELETE ANIM!
-    /*override fun onStart() {
+    override fun onStart() {
         super.onStart()
         binding.loco.setBackgroundResource(R.drawable.animate_list)
         locoCat =  binding.loco.background as AnimationDrawable
-        locoCat.start()
 
-    }*/
+    }
 
     private fun goToTimePicker() {
         val intent = android.content.Intent(this@MainActivity, TimePicker::class.java)
@@ -86,8 +85,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun startTimer() {
         //DONt
-        /*if (locoCat.isRunning) locoCat.stop()
-        else locoCat.start()*/
+        locoCat.start()
+
 
         serviceIntent.putExtra(TimerService.TIME_EXTRA, time)
         startService(serviceIntent)
@@ -97,8 +96,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun stopTimer() {
-       /* if (locoCat.isRunning)
-            locoCat.stop()*/
+        if (locoCat.isRunning)
+            locoCat.stop()
 
 
         stopService(serviceIntent)
