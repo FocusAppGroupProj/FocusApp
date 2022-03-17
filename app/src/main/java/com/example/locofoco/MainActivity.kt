12d78a07f004fe32965e45e489b8c55d0ac5d274
waterFalt777 <stephy888la7@gmail.com>
2022-03-16 23:42:43 -0700

    hopefully this commits right

--- app/src/main/java/com/example/locofoco/MainActivity.kt
+++ app/src/main/java/com/example/locofoco/MainActivity.kt
@@ -61,7 +61,14 @@ class MainActivity : AppCompatActivity() {
         }
 
         //gallery bindings
-        binding.galler
+        binding.galleryButton.setOnClickListener {
+            goToGalleryActivity()
+        }
+        loadImages()
+
+        binding.launchImg.setOnClickListener{
+            getCatImageUrl()
+        }
 
         serviceIntent = Intent1(applicationContext, TimerService::class.java)
         registerReceiver(updateTime, IntentFilter(TimerService.TIMER_UPDATED))
@@ -149,87 +156,72 @@ class MainActivity : AppCompatActivity() {
         const val TAG = "MainActivity"
     }
 
+    //GALLERY FUN
+    private fun getCatImageUrl(){
+        client.get(CAT_IMAGE_URL, object: JsonHttpResponseHandler(){
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                response: String?,
+                throwable: Throwable?
+            ) {
+                Log.e(TAG, "onFailure $statusCode")
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
+                //This function returns an image url from the json response
+                Log.i(TAG, "onSuccess: JSON data ${json.jsonArray.getJSONObject(0).getString("url")}")
+                //TODO: check dimensions of the image
+                try {
+                    img_url = json.jsonArray.getJSONObject(0).getString("url")
+                    //val cat_img = CatImage(img_url)
+                    imageUrl_list.add(img_url)
+                    catImgs.add(CatImage(img_url))
+                    saveUrls()
+                    popUpCatImage(img_url) //popup the cat image
+                }catch(e: JSONException){
+                    Log.e(TAG, "Encountered exception $e")
+                }
+            }
+        })
+    }
+
+    private fun popUpCatImage(img_url: String){
+        Log.i(TAG, "pressing button, img_url is $img_url")
+        val intent = Intent1(this@MainActivity, PopUpWindow::class.java)
+        intent.putExtra("img_url", img_url)
+        startActivity(intent)
+    }
+
+    private fun goToGalleryActivity(){
+        val intent = Intent1(this@MainActivity, GalleryActivity::class.java)
+        intent.putParcelableArrayListExtra("cat_imgs",catImgs)
+        startActivity(intent)
+
+    }
+
+    //save image url
+    fun getDataFile() : File {
+        return File(filesDir,"catUrls.txt")
+    }
+    fun loadImages() {
+        try {
+            imageUrl_list = FileUtils.readLines(getDataFile()) as MutableList<String>
+            for (i in 0 until imageUrl_list.size){
+                catImgs.add(CatImage(imageUrl_list.get(i)))
+            }
+        } catch (ioExceptioin: IOException){
+            ioExceptioin.printStackTrace()
+        }
+    }
+    fun saveUrls(){
+        try{
+            FileUtils.writeLines(getDataFile(),imageUrl_list)
+        } catch (ioExceptioin: IOException){
+            ioExceptioin.printStackTrace()
+        }
+    }
 
 
-//        findViewById<Button>(R.id.gallery_button).setOnClickListener{
-//            goToGalleryActivity()
-//        }
-//        loadImages()
-//
-//
-//        findViewById<Button>(R.id.launch_img).setOnClickListener {
-//            getCatImageUrl()
-//        }
-//
-//        Log.i(TAG, "returning from POpUpWIndow to MainActivity")
-//    }
-//
-//    private fun getCatImageUrl(){
-//        client.get(CAT_IMAGE_URL, object: JsonHttpResponseHandler(){
-//            override fun onFailure(
-//                statusCode: Int,
-//                headers: Headers?,
-//                response: String?,
-//                throwable: Throwable?
-//            ) {
-//                Log.e(TAG, "onFailure $statusCode")
-//            }
-//
-//            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
-//                //This function returns an image url from the json response
-//                Log.i(TAG, "onSuccess: JSON data ${json.jsonArray.getJSONObject(0).getString("url")}")
-//                //TODO: check dimensions of the image
-//                try {
-//                    img_url = json.jsonArray.getJSONObject(0).getString("url")
-//                    //val cat_img = CatImage(img_url)
-//                    imageUrl_list.add(img_url)
-//                    catImgs.add(CatImage(img_url))
-//                    saveUrls()
-//                    popUpCatImage(img_url) //popup the cat image
-//                }catch(e: JSONException){
-//                    Log.e(TAG, "Encountered exception $e")
-//                }
-//            }
-//        })
-//    }
-//
-//    private fun popUpCatImage(img_url: String){
-//        Log.i(TAG, "pressing button, img_url is $img_url")
-//        val intent = Intent(this@MainActivity, PopUpWindow::class.java)
-//        intent.putExtra("img_url", img_url)
-//        startActivity(intent)
-//    }
-//
-//
-//    private fun goToGalleryActivity(){
-//        val intent = Intent(this@MainActivity, GalleryActivity::class.java)
-//        intent.putParcelableArrayListExtra("cat_imgs",catImgs)
-//        startActivity(intent)
-//
-//    }
-//
-//    //save image url
-//    fun getDataFile() : File {
-//        return File(filesDir,"catUrls.txt")
-//    }
-//    fun loadImages() {
-//        try {
-//            imageUrl_list = FileUtils.readLines(getDataFile()) as MutableList<String>
-//            for (i in 0 until imageUrl_list.size){
-//                catImgs.add(CatImage(imageUrl_list.get(i)))
-//            }
-//        } catch (ioExceptioin: IOException){
-//            ioExceptioin.printStackTrace()
-//        }
-//    }
-//
-//    fun saveUrls(){
-//        try{
-//            FileUtils.writeLines(getDataFile(),imageUrl_list)
-//        } catch (ioExceptioin: IOException){
-//            ioExceptioin.printStackTrace()
-//        }
-//    }
-//
 
 }
