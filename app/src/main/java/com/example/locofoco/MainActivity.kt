ccadf4a1124865f7829fc79532551f8b9b1b4fc2
waterFalt777 <stephy888la7@gmail.com>
2022-03-22 13:32:57 -0700

    merged timer zero and format fix

--- app/src/main/java/com/example/locofoco/MainActivity.kt
+++ app/src/main/java/com/example/locofoco/MainActivity.kt
@@ -7,13 +7,13 @@ import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Toast
-import androidx.appcompat.app.ActionBar
+//import android.widget.Toast
+//import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import com.example.locofoco.databinding.ActivityMainBinding
-import com.example.locofoco.databinding.ActivityPopupWindowBinding
+//import com.example.locofoco.databinding.ActivityPopupWindowBinding
 import okhttp3.Headers
 import org.apache.commons.io.FileUtils
 import org.json.JSONException
@@ -41,6 +41,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var timeStarted = false
     private lateinit var serviceIntent: Intent1
+
     //private lateinit var intent: Intent1
     private var time = 0
     private var start_time = 0
@@ -50,7 +51,8 @@ class MainActivity : AppCompatActivity() {
 
     //gallery
     var imageUrl_list = mutableListOf<String>()
-//    var catImgs =   ArrayList<CatImage>()
+
+    //    var catImgs =   ArrayList<CatImage>()
     private val client = AsyncHttpClient()
     private var img_url = ""
     private var updated = false
@@ -68,22 +70,23 @@ class MainActivity : AppCompatActivity() {
         binding.SetTime.setOnClickListener {
             goToTimePicker()
         }
-        binding.start.setOnClickListener{
+        binding.start.setOnClickListener {
             startStopTimer()
         }
-        binding.Reset.setOnClickListener{
+        binding.Reset.setOnClickListener {
             resetTimer()
         }
 
         serviceIntent = Intent1(applicationContext, TimerService::class.java)
         registerReceiver(updateTime, IntentFilter(TimerService.TIMER_UPDATED))
 
-        }
+    }
+
     //DONT DELETE ANIM!
     override fun onStart() {
         super.onStart()
-           binding.loco.setBackgroundResource(R.drawable.animate_list)
-           locoCat =  binding.loco.background as AnimationDrawable
+        binding.loco.setBackgroundResource(R.drawable.animate_list)
+        locoCat = binding.loco.background as AnimationDrawable
         //popUpBinding.locoPop.setBackgroundResource(R.drawable.animate_list)
         //locoPopCat =  popUpBinding.locoPop.background as AnimationDrawable
 
@@ -103,12 +106,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startStopTimer() {
-        if (timeStarted){
+        if (timeStarted) {
             stopTimer()
-        }
-        else{
+        } else {
             startTimer()
             binding.Reset.visibility = View.VISIBLE
+            binding.Reset.setTextColor(getResources().getColor(R.color.teal_900))
         }
     }
 
@@ -123,7 +126,7 @@ class MainActivity : AppCompatActivity() {
         binding.start.background = getDrawable(R.drawable.simp_r_btn)
         //binding.start.pointerIcon = getDrawable(R.drawable.ic_baseline_pause_24)
         timeStarted = true
-        if (updated){
+        if (updated) {
             updated = false
         }
 
@@ -142,103 +145,111 @@ class MainActivity : AppCompatActivity() {
         timeStarted = false
     }
 
-    private val updateTime: BroadcastReceiver = object: BroadcastReceiver(){
+    private val updateTime: BroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent1) {
             time = intent.getIntExtra(TimerService.TIME_EXTRA, 0)
             binding.Timer.text = getTimeStringFromInt(time)
             var str_time = time.toString()
-            if (time == 0){
+            if (time == 0) {
                 Log.i(TAG, "time:$time")
                 resetTimer()
-                if (!updated){
+                if (!updated) {
                     getCatImageUrl()
                     updated = true
                 }
             }
 
+        }
     }
 
-    private fun getTimeStringFromInt(time: Int): String {
-        val resultInt = time
-        val hours = resultInt % 86400 / 3600
-        val minutes = resultInt % 86400 % 3600 / 60
-        val seconds = resultInt % 86400 % 3600 % 60
+        private fun getTimeStringFromInt(time: Int): String {
+            val resultInt = time
+            val hours = resultInt % 86400 / 3600
+            val minutes = resultInt % 86400 % 3600 / 60
+            val seconds = resultInt % 86400 % 3600 % 60
 
-        return makeTimeString(hours, minutes, seconds)
+            return makeTimeString(hours, minutes, seconds)
 
-    }
+        }
 
-    private fun makeTimeString(hours: Int, minutes: Int, seconds: Int): String = String.format("%02d:%02d:%02d", hours, minutes, seconds )
 
-    companion object{
-        const val TAG = "MainActivity"
-    }
+        private fun makeTimeString(hours: Int, minutes: Int, seconds: Int): String =
+            String.format("%02d:%02d:%02d", hours, minutes, seconds)
 
-    //GALLERY FUN
-    private fun getCatImageUrl(){
-        client.get(CAT_IMAGE_URL, object: JsonHttpResponseHandler(){
-            override fun onFailure(
-                statusCode: Int,
-                headers: Headers?,
-                response: String?,
-                throwable: Throwable?
-            ) {
-                Log.e(TAG, "onFailure $statusCode")
-            }
+//    companion object{
+//        const val TAG = "MainActivity"
+//    }
 
-            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
-                //This function returns an image url from the json response
-                Log.i(TAG, "onSuccess: JSON data ${json.jsonArray.getJSONObject(0).getString("url")}")
-                //TODO: check dimensions of the image
-                try {
-                    img_url = json.jsonArray.getJSONObject(0).getString("url")
-                    loadImages() //update url list in case there is any image that has been deleted
-                    imageUrl_list.add(img_url)
-//                    catImgs.add(CatImage(img_url))
-                    saveUrls()
-                    popUpCatImage(img_url) //popup the cat image
-
-                }catch(e: JSONException){
-                    Log.e(TAG, "Encountered exception $e")
+        //GALLERY FUN
+        private fun getCatImageUrl() {
+            client.get(CAT_IMAGE_URL, object : JsonHttpResponseHandler() {
+                override fun onFailure(
+                    statusCode: Int,
+                    headers: Headers?,
+                    response: String?,
+                    throwable: Throwable?
+                ) {
+                    Log.e(TAG, "onFailure $statusCode")
                 }
-            }
-        })
-    }
 
-    private fun popUpCatImage(img_url: String){
-        Log.i(TAG, "pressing button, img_url is $img_url")
-        val intent = Intent1(this@MainActivity, PopUpWindow::class.java)
-        intent.putExtra("img_url", img_url)
-        startActivity(intent)
-    }
+                override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
+                    //This function returns an image url from the json response
+                    Log.i(
+                        TAG,
+                        "onSuccess: JSON data ${json.jsonArray.getJSONObject(0).getString("url")}"
+                    )
+                    //TODO: check dimensions of the image
+                    try {
+                        img_url = json.jsonArray.getJSONObject(0).getString("url")
+                        loadImages() //update url list in case there is any image that has been deleted
+                        imageUrl_list.add(img_url)
+//                    catImgs.add(CatImage(img_url))
+                        saveUrls()
+                        popUpCatImage(img_url) //popup the cat image
 
-    private fun goToGalleryActivity(){
-        val intent = Intent1(this@MainActivity, GalleryActivity::class.java)
+                    } catch (e: JSONException) {
+                        Log.e(TAG, "Encountered exception $e")
+                    }
+                }
+            })
+        }
+
+        private fun popUpCatImage(img_url: String) {
+            Log.i(TAG, "pressing button, img_url is $img_url")
+            val intent = Intent1(this@MainActivity, PopUpWindow::class.java)
+            intent.putExtra("img_url", img_url)
+            startActivity(intent)
+        }
+
+        private fun goToGalleryActivity() {
+            val intent = Intent1(this@MainActivity, GalleryActivity::class.java)
 //        intent.putParcelableArrayListExtra("cat_imgs",catImgs)
-        startActivity(intent)
-    }
+            startActivity(intent)
+        }
 
-    //save image url
-    fun getDataFile() : File {
-        return File(filesDir,"catUrls.txt")
-    }
-    fun loadImages() {
-        try {
-            imageUrl_list = FileUtils.readLines(getDataFile()) as MutableList<String>
+        //save image url
+        fun getDataFile(): File {
+            return File(filesDir, "catUrls.txt")
+        }
+
+        fun loadImages() {
+            try {
+                imageUrl_list = FileUtils.readLines(getDataFile()) as MutableList<String>
 //            for (i in 0 until imageUrl_list.size){
 //                catImgs.add(CatImage(imageUrl_list.get(i)))
 //            }
-        } catch (ioExceptioin: IOException){
-            ioExceptioin.printStackTrace()
+            } catch (ioExceptioin: IOException) {
+                ioExceptioin.printStackTrace()
+            }
         }
-    }
-    fun saveUrls(){
-        try{
-            FileUtils.writeLines(getDataFile(),imageUrl_list)
-        } catch (ioException: IOException){
-            ioException.printStackTrace()
+
+        fun saveUrls() {
+            try {
+                FileUtils.writeLines(getDataFile(), imageUrl_list)
+            } catch (ioException: IOException) {
+                ioException.printStackTrace()
+            }
         }
-    }
 
 
-}
+    }
